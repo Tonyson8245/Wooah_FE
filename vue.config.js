04bdf0cf6baab59f78bf05e61857dfd7867fc2fb
Tonyson <dongo8245@gmail.com>
2022-04-27 19:38:14 +0900

    Change build path

--- vue.config.js
+++ vue.config.js
@@ -37,7 +37,7 @@
 // };
 
 module.exports = {
-  outputDir: "D:/응용 2단게/project/frontend-web-production",
+  outputDir: "D:/응용 2단게/project/frontend-web-production/dist",
 
   //   publicPath: "./", //해당 설정을 추가 해줍니다. 상대경로인 ./ 로 설정 합니다.
   //   assetsDir: "./", //만약 css, js, img 등 어셋 경로가 다를 경우 해당 경로도 수정 해야 합니다.
