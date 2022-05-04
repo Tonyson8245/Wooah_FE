8e8f285019198835faf2a28e055ee0cbd966f904
Tonyson <dongo8245@gmail.com>
2022-05-04 17:46:59 +0900

    Add banner
    이달의 아트 상단 배너 제작 완료

--- src/components/MonthlyartPage/MonthlyartPage.vue
+++ src/components/MonthlyartPage/MonthlyartPage.vue
@@ -1,10 +1,29 @@
-<template>Monthlyart</template>
+<template>
+  <div>
+    <banner :title="title" />
+  </div>
+</template>
 
 <script>
+import banner from "./BannerComponent.vue";
+
 export default {
   name: `Monthlyart`,
+  data() {
+    return {};
+  },
+  computed: {
+    title() {
+      var title;
+      if (this.$route.path == "/monthlyart") title = "monthlyart";
+      return title;
+    },
+  },
   mounted() {
     this.$store.commit("Setpagecondition", "monthlyart");
+  },
+  components: {
+    banner,
   },
 };
 </script>
